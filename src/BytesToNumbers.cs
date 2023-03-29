54fd7b10ee4f4abfa542b3eb1599d0c5a4eb1031
Kaarlo Räihä <mcraiha@users.noreply.github.com>
2023-03-29 21:17:16 +0300

    Add BytesToNumbers class
    For bytes to numbers conversion

---
+++ src/BytesToNumbers.cs
@@ -0,0 +1,83 @@
+using System;
+using System.Buffers.Binary;
+
+namespace Codec8
+{
+    /// <summary>
+    /// Turn bytes to numbers (static class)
+    /// </summary>
+    public static class BytesToNumbers
+    {
+        /// <summary>
+        /// Get Int16 from two bytes
+        /// </summary>
+        /// <param name="bytes">Bytes</param>
+        /// <returns>Int16</returns>
+        public static short GetInt16(ReadOnlySpan<byte> bytes)
+        {
+            short returnValue = BitConverter.ToInt16(bytes);
+
+            // Change endianness if needed
+            if (BitConverter.IsLittleEndian)
+            {
+                returnValue = BinaryPrimitives.ReverseEndianness(returnValue);
+            }
+
+            return returnValue;
+        }
+
+        /// <summary>
+        /// Get UInt16 from two bytes
+        /// </summary>
+        /// <param name="bytes">Bytes</param>
+        /// <returns>UInt16</returns>
+        public static ushort GetUInt16(ReadOnlySpan<byte> bytes)
+        {
+            ushort returnValue = BitConverter.ToUInt16(bytes);
+
+            // Change endianness if needed
+            if (BitConverter.IsLittleEndian)
+            {
+                returnValue = BinaryPrimitives.ReverseEndianness(returnValue);
+            }
+
+            return returnValue;
+        }
+
+        /// <summary>
+        /// Get Int32 from four bytes
+        /// </summary>
+        /// <param name="bytes">Bytes</param>
+        /// <returns>Int32</returns>
+        public static int GetInt32(ReadOnlySpan<byte> bytes)
+        {
+            int returnValue = BitConverter.ToInt32(bytes);
+
+            // Change endianness if needed
+            if (BitConverter.IsLittleEndian)
+            {
+                returnValue = BinaryPrimitives.ReverseEndianness(returnValue);
+            }
+
+            return returnValue;
+        }
+
+        /// <summary>
+        /// Get UInt32 from four bytes
+        /// </summary>
+        /// <param name="bytes">Bytes</param>
+        /// <returns>UInt32</returns>
+        public static uint GetUInt32(ReadOnlySpan<byte> bytes)
+        {
+            uint returnValue = BitConverter.ToUInt32(bytes);
+
+            // Change endianness if needed
+            if (BitConverter.IsLittleEndian)
+            {
+                returnValue = BinaryPrimitives.ReverseEndianness(returnValue);
+            }
+
+            return returnValue;
+        }
+    }
+}
